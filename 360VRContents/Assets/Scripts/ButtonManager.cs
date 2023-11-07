054292c1a6d66c44ca8805aacc9ab101374932f8
bongbub <whgusqls5@naver.com>
2023-11-07 21:37:13 +0900

    작업 마무리 후 업로등

---
+++ 360VRContents/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    public GameObject Sphere;
+    public VideoClip Clip1;
+    public VideoClip Clip2;
+
+    public void ToVideoScene()
+    {
+        SceneManager.LoadScene("360Play");
+    }
+
+    public void GoIn()
+    {
+        Sphere.GetComponent<VideoPlayer>().clip = Clip2;
+    }
+    public void GoOut()
+    {
+        Sphere.GetComponent<VideoPlayer>().clip = Clip1;
+    }
+
+}
